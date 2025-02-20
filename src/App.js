495164c943e09b364fb3020ee5edeed7bc8179cf
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-19 19:40:48 -0600

    routeDefinitions

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider,
+} from "react-router-dom";
 import Home from "./pages/Home";
 import Product from "./pages/Product";
 
-const router = createBrowserRouter([
-  { path: "/", element: <Home /> },
-  { path: "/products", element: <Product /> },
-]);
+// const router = createBrowserRouter([
+//   { path: "/", element: <Home /> },
+//   { path: "/products", element: <Product /> },
+// ]);
+
+const routeDefinitions = createRoutesFromElements(
+  <Route>
+    <Route path="/" element={<Home />}></Route>
+    <Route path="/products" element={<Product />}></Route>
+  </Route>
+);
+const router = createBrowserRouter(routeDefinitions)
 function App() {
   return <RouterProvider router={router} />;
 }
