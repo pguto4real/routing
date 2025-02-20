77e7eb2f42910f5dc486976a353718fb05c9397a
oluwamuibiclear <igbokweudoka@gmail.com>
2025-02-19 19:36:59 -0600

    Product

--- src/App.js
+++ src/App.js
@@ -1,5 +1,13 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Home from "./pages/Home";
+import Product from "./pages/Product";
+
+const router = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/products", element: <Product /> },
+]);
 function App() {
-  return <div></div>;
+  return <RouterProvider router={router} />;
 }
 
 export default App;
